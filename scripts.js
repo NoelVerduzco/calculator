1e780a617e83005204ab99c4561370eb68d7b1ff
noelverduzco <noeljverduzco02@gmail.com>
2023-02-14 00:05:29 -0800

    Make get-x, get-y, get-operator, and calculation functionality

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-// create add, subtract, multiply, and divide functions
-
 function add(x, y) {
     return x + y;
 }
@@ -18,23 +16,81 @@ function divide(x, y) {
 }
 
 function operate(operation, x, y) {
-    if (operation === add) return add(x, y);
-    if (operation === subtract) return subtract(x, y);
-    if (operation === multiply) return multiply(x, y);
-    if (operation === divide) return divide(x, y);
+    if (operation === "add") return add(x, y);
+    if (operation === "subtract") return subtract(x, y);
+    if (operation === "multiply") return multiply(x, y);
+    if (operation === "divide") return divide(x, y);
 }
 
-// function penis_size(mines,yours) {
-//     return mines + yours;
-// }
+const numbers = document.querySelectorAll(".number");
 
-const buttons = document.querySelectorAll("button");
+let xNum = null;
+let xList = [];
+
+function getX() {
+
+    numbers.forEach(number => {
+        number.addEventListener("click", function() {
+            xList.push(number.innerText);
+            // history.innerText = xList.join("");
+        });
+    });
+}
+
+let yNum = null;
+let yList = [];
+
+function getY() {
+    
+    numbers.forEach(number => {
+        number.addEventListener("click", function() {
+            yList.push(number.innerText);
+            // history.innerText = yList.join("");
+        });
+    });
+}
+
+let sign = null;
+
+function getOperator() {
+    operators.forEach(operator => {
+        operator.addEventListener("click", function() {
+            xNum = Number(xList.join(""));
+            yNum = Number(yList.join(""));
+            sign = operator.id;
+            calculate();
+        });
+    });
+}
+
+const operators = document.querySelectorAll(".operator");
 
 const screen = document.querySelector(".screen");
 
-buttons.forEach(button => {
-    button.addEventListener("click", function(){
-        console.log(button.innerText);
-        screen.innerText = button.innerText;
+const history = document.querySelector("#history");
+
+const clear = document.querySelector("#clear");
+
+let total = null;
+
+function calculate() {
+    if (sign === null) {
+        getX();
+        getOperator();
+    }
+    if ((sign !== null) && (xNum !== null)) {
+        getY();
+    }
+    equals.addEventListener("click", function() {
+        yNum = Number(yList.join(""));
+        console.log(yNum);
+        if ((sign !== null) && (xNum !== null) && (yNum !== null)) {
+            total = operate(sign, xNum, yNum);
+            console.log(total);
+        }
     });
-});
+}
+
+const equals = document.querySelector("#equals");
+
+calculate();
