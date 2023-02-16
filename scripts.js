6d346fbe8da74b6d50ca501b355c80d6b53cb7e4
noelverduzco <noeljverduzco02@gmail.com>
2023-02-15 22:33:42 -0800

    New perspective

--- scripts.js
+++ scripts.js
@@ -24,45 +24,6 @@ function operate(operation, x, y) {
 
 const numbers = document.querySelectorAll(".number");
 
-let xNum = null;
-let xList = [];
-
-function getX() {
-
-    numbers.forEach(number => {
-        number.addEventListener("click", function() {
-            xList.push(number.innerText);
-            // history.innerText = xList.join("");
-        });
-    });
-}
-
-let yNum = null;
-let yList = [];
-
-function getY() {
-    
-    numbers.forEach(number => {
-        number.addEventListener("click", function() {
-            yList.push(number.innerText);
-            // history.innerText = yList.join("");
-        });
-    });
-}
-
-let sign = null;
-
-function getOperator() {
-    operators.forEach(operator => {
-        operator.addEventListener("click", function() {
-            xNum = Number(xList.join(""));
-            yNum = Number(yList.join(""));
-            sign = operator.id;
-            calculate();
-        });
-    });
-}
-
 const operators = document.querySelectorAll(".operator");
 
 const screen = document.querySelector(".screen");
@@ -71,26 +32,155 @@ const history = document.querySelector("#history");
 
 const clear = document.querySelector("#clear");
 
-let total = null;
-
-function calculate() {
-    if (sign === null) {
-        getX();
-        getOperator();
-    }
-    if ((sign !== null) && (xNum !== null)) {
-        getY();
-    }
-    equals.addEventListener("click", function() {
-        yNum = Number(yList.join(""));
-        console.log(yNum);
-        if ((sign !== null) && (xNum !== null) && (yNum !== null)) {
-            total = operate(sign, xNum, yNum);
-            console.log(total);
-        }
-    });
-}
-
 const equals = document.querySelector("#equals");
 
-calculate();
+// CALL getX IF sign is null
+// ADD listeners for X number clicks
+// APPEND number clicks to array
+// JOIN array to make a string of numbers
+// SET xNum value
+// REMOVE listeners for X number clicks
+// CALL calculate
+
+let xNum = null;
+let xList = [];
+// let isX = false;
+
+function getX() {
+    numbers.forEach(number => {
+        number.addEventListener("click", function() {
+            xList.push(number.innerText);
+            calculate();
+            // isX = true;
+        });
+    });
+    numbers.forEach(number => {
+            number.removeEventListener("click", function() {
+                    xList.push(number.innerText);
+            xNum = Number(xList.join(""));
+            console.log(xNum);
+            isX = true;
+        });
+    });
+    while (isX === true) {
+            isX = false;
+            calculate();
+        }
+    }
+    
+    // CALL getOperator IF X is NOT null AND IF sign is null
+    // ADD listeners for operator clicks
+    // SET sign value
+    // REMOVE listeners for operator clicks
+    // CALL calculate
+    
+    let sign = null;
+    // let isSign = false;
+    
+    function getOperator() {
+        operators.forEach(operator => {
+            operator.addEventListener("click", function() {
+                sign = operator.id;
+                console.log(sign);
+                // isSign = true;
+                xNum = Number(xList.join(""));
+                console.log(xNum);
+            });
+        });
+        // operators.forEach(operator => {
+            //     operator.removeEventListener("click", function() {
+                //         sign = operator.id;
+                //         console.log(sign);
+                //         isSign = true;
+                //     });
+                // });
+                // while (isSign === true) {
+                //     isSign = false;
+                //     calculate();
+                // }
+    }
+
+            // CALL getY IF X is NOT null AND IF sign is NOT null
+            // ADD listeners for Y number clicks
+            // APPEND number clicks to array
+            // JOIN array to make a string of numbers
+            // SET yNum value
+            // REMOVE listeners for Y number clicks
+            // CALL calculate
+            
+            let yNum = null;
+            let yList = [];
+            let isY = false;
+            
+            function getY() {
+                numbers.forEach(number => {
+        number.addEventListener("click", function() {
+            yList.push(number.innerText);
+            yNum = Number(yList.join(""));
+            console.log(yNum);
+            isY = true;
+        });
+    });
+    // numbers.forEach(number => {
+        //     number.removeEventListener("click", function() {
+    //         yList.push(number.innerText);
+    //         yNum = Number(yList.join(""));
+    //         console.log(yNum);
+    //         isY = true;
+    //     });
+    // });
+    while (isY === true) {
+        isY = false;
+        calculate();
+    }
+}
+
+// CALL getEquals IF X is NOT null AND IF sign is NOT null AND if Y is NOT null
+// ADD listener for equals click
+// CALL operate and pass xNum, yNum, and sign
+// LOG operate
+// SET xNum to yNum
+// SET yNum to null
+// SET sign to null
+// SET xList to empty array
+// SET yList to empty array
+
+let total = null;
+
+function getEquals() {
+    equals.addEventListener("click", function() {
+        total = operate(sign, xNum, yNum);
+        console.log(total);
+        xNum = yNum;
+        yNum = null;
+        sign = null;
+        xList = [];
+        yList = [];
+    });
+    // equals.removeEventListener("click", function() {
+        //     total = operate(sign, xNum, yNum);
+        //     console.log(total);
+        //     xNum = yNum;
+        //     yNum = null;
+        //     sign = null;
+        //     xList = [];
+        //     yList = [];
+        // });
+    }
+    
+    function calculate() {
+        if ((sign === null) && (xNum === null)) {
+            getX();
+        }
+        if ((sign === null) && (xNum !== null)) {
+            getOperator();
+        }
+        if ((sign !== null) && (xNum !== null)) {
+            getY();
+        }
+        if ((sign !== null) && (xNum !== null) && (yNum !== null)) {
+            getEquals();
+        }
+    }
+    
+    calculate();
